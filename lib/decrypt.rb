130f67a92991b6ec1cde8321b97b97b4018678dc
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-13 09:41:33 -0600

    Update variable names in runner

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,8 +2,8 @@ require_relative './enigma'
 enigma = Enigma.new
 input = ARGV[0]
 output = ARGV[1]
-key_input = ARGV[2]
-date_input = ARGV[3]
+key = ARGV[2]
+date = ARGV[3]
 decryption = enigma.decrypt(File.read(input).tr("\n", ""), key, date)
 File.open(output, "w") do |file|
   file.puts decryption[:decryption]
