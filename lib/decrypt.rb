549b6601c23d2adde95ef004272540be7794698c
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-13 08:59:00 -0600

    Update variable naming

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,7 +4,7 @@ input = ARGV[0]
 output = ARGV[1]
 key_input = ARGV[2]
 date_input = ARGV[3]
-decryption = enigma.decrypt(File.read(input).tr("\n", ""), key_input, date_input)
+decryption = enigma.decrypt(File.read(input).tr("\n", ""), key, date)
 File.open(output, "w") do |file|
   file.puts decryption[:decryption]
   puts "create #{output} with the key #{decryption[:key]} and date #{decryption[:date]}"
