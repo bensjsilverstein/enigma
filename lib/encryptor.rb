9fa86f8a354f11c6f541406b372e8073885d5935
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-14 13:16:26 -0600

    Add tests for Rotatables module

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -10,35 +10,7 @@ class Encryptor
     @key = key
     @date = date
     @alphabet = ("a".."z").to_a << " "
-    @encrypted_hash = {}
-    @decrypted_hash = {}
   end
-
-  # def key_generator
-  #   ('%05d' % rand(10 ** 4))
-  # end
-  #
-  # def date_setup
-  #   @date = (Time.now).strftime("%d%m%y")
-  # end
-  #
-  # def shift_calculator
-  #   last_four_digits = (@date.to_i**2).to_s[-4..-1]
-  #   @a_key = @key[0..1].to_i
-  #   @b_key = @key[1..2].to_i
-  #   @c_key = @key[2..3].to_i
-  #   @d_key = @key[3..4].to_i
-  #   @a_offset = last_four_digits[0].to_i
-  #   @b_offset = last_four_digits[1].to_i
-  #   @c_offset = last_four_digits[2].to_i
-  #   @d_offset = last_four_digits[3].to_i
-  #
-  #   @a_shift = @a_key + @a_offset
-  #   @b_shift = @b_key + @b_offset
-  #   @c_shift = @c_key + @c_offset
-  #   @d_shift = @d_key + @d_offset
-  # end
-
 
   def encrypt
     message_array = @message.downcase.chars
@@ -53,7 +25,7 @@ class Encryptor
         char = d_rotation[char]
       end
     end
-    @encrypted_hash = {:encryption => encrypted.join, :key => @key, :date => @date}
+    encrypted_hash = {:encryption => encrypted.join, :key => @key, :date => @date}
   end
 
   def decrypt
@@ -69,7 +41,7 @@ class Encryptor
         char = reverse_d[char]
       end
     end
-    @decrypted_hash = {:decryption => message.join, :key => @key, :date => @date}
+    decrypted_hash = {:decryption => message.join, :key => @key, :date => @date}
   end
 
 end
--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -13,16 +13,34 @@ RSpec.describe Encryptor do
   it "has a message" do
     expect(@encryptor.message).to eq "hello world"
   end
+
   it "has a key" do
     expect(@encryptor.key).to eq "02715"
   end
+
   it "has a date" do
     expect(@encryptor.date).to eq "040895"
   end
+
   it "has an array of characters to shift to" do
+    expect(@encryptor.alphabet).to be_a Array
     expect(@encryptor.alphabet.size).to eq 27
   end
 
+  it "can encrypt" do
+    expect(@encryptor.encrypt).to eq({
+      :encryption => "keder ohulw",
+      :key => "02715",
+      :date => "040895"})
+  end
+
+  it "can decrypt" do
+    expect(@decryptor.decrypt).to eq({
+      :decryption => "hello world",
+      :key => "02715",
+      :date => "040895"})
+  end
+  # ====== Shiftables Module tests(below) ======
   it "can generate a key" do
     expect(@encryptor.key_generator).to be_a(String)
     expect(@encryptor.key_generator.length).to eq(5)
@@ -37,30 +55,25 @@ RSpec.describe Encryptor do
     expect(@encryptor.shift_calculator).to be_a Integer
   end
 
-  it "has an alphabet" do
-    expect(@encryptor.alphabet).to be_a Array
-    expect(@encryptor.alphabet.size).to eq 27
+  # ====== Rotatables Module tests(below) ======
+  it "has rotated alphabets" do
+    expect(@encryptor.a_rotation).to be_a Hash
+    expect(@encryptor.a_rotation.first).to eq(["a", "d"])
+    expect(@encryptor.a_rotation.count).to eq(27)
+
+    expect(@encryptor.b_rotation).to be_a Hash
+    expect(@encryptor.b_rotation.first).to eq(["a", "a"])
+    expect(@encryptor.b_rotation.count).to eq(27)
+
+    expect(@encryptor.c_rotation).to be_a Hash
+    expect(@encryptor.c_rotation.first).to eq(["a", "t"])
+    expect(@encryptor.c_rotation.count).to eq(27)
+
+    expect(@encryptor.d_rotation).to be_a Hash
+    expect(@encryptor.d_rotation.first).to eq(["a", "u"])
+    expect(@encryptor.d_rotation.count).to eq(27)
+
   end
 
-  it "can generate a random 5 digit number" do
-    expect(@encryptor.key).to be_a String
-    expect(@encryptor.key.length).to eq 5
-  end
-
-  it "can encrypt" do
-    # require "pry"; binding.pry
-    expect(@encryptor.encrypt).to eq({
-      :encryption => "keder ohulw",
-      :key => "02715",
-      :date => "040895"})
-  end
-
-  it "can decrypt" do
-    # require "pry"; binding.pry
-    expect(@decryptor.decrypt).to eq({
-      :decryption => "hello world",
-      :key => "02715",
-      :date => "040895"})
-  end
 
 end
