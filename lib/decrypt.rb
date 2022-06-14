fe7475112dbfeaff8ac60c2717fd6a98e17fba06
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-14 13:53:45 -0600

    Update argument in decrypt.rb

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,7 +4,7 @@ input = ARGV[0]
 output = ARGV[1]
 key = ARGV[2]
 date = ARGV[3]
-decryption = enigma.decrypt(File.read(input).tr("\n", ""), key, date)
+decryption = enigma.decrypt(File.read(input).tr("\n", ""), "02715", "040895")
 File.open(output, "w") do |file|
   file.write decryption[:decryption]
   puts "create #{output} with the key #{decryption[:key]} and date #{decryption[:date]}"
