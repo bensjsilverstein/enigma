31d66e5e676d2be4f2b1a76e0b2713afcca19b71
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-14 13:52:34 -0600

    Update require_relative statements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'simplecov'
 SimpleCov.start
 
-require_relative 'cipher_spec'
-require_relative 'decryptor_spec'
 require_relative 'encryptor_spec'
 require_relative 'enigma_spec'
 # require_relative 'enigma_spec'
