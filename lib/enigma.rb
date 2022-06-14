f50f12f3651ff3b6e06a678e573d12fa2badc7d6
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-14 14:28:54 -0600

    Remove initialize

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,22 +4,12 @@ require './lib/encryptor'
 class Enigma
   include Rotatables, Shiftables
 
-  def initialize
-
-  end
-
   def encrypt(message, key = key_generator, date = date_setup)
     @encryption = Encryptor.new(message, key, date).encrypt
   end
 
-  def decrypt(message, key = key_generator, date = date_setup)
+  def decrypt(message, key, date = date_setup)
     @decryption = Encryptor.new(message, key, date).decrypt
   end
-
-  def date_setup
-    @date = (Time.now).strftime("%d%m%y")
-  end
-
-
 
 end
